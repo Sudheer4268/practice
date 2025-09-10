4d93db4e7df8031cbd59bdbc343c6cf1788e3412
Sudheer Kumar Goddumarri <Sudheer.g@EIS.LOCAL>
2025-09-10 19:00:21 +0530

    first commit

---
+++ practice.js
@@ -0,0 +1,1 @@
+console.log("Hello, Sudheer! JavaScript is running 🚀");
---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("Hello, JavaScript is working 🚀");
+
+let arr=[1,20];
+console.log(arr+1);
+
+{
+    let arr=[2,4]-1;
+    console.log(arr);
+}
+
+console.log([7,2]*2)
+
+function greet(name){
+    return("Hello "+name+ "!");
+}
+console.log(greet("sudheer kumar"));
+
+
+function add(a,b){
+    return(a*b);
+}
+console.log(add(6,8));
